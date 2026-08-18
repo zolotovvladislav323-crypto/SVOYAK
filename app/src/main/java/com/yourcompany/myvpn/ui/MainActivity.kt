9f61e6c48bc2534247570f8c5244489ae9e9c927
zolotovvladislav323-crypto <zolotovvladislav323@gmail.com>
2026-08-18 15:04:53 +0300

    Update MainActivity.kt

--- app/src/main/java/com/yourcompany/myvpn/ui/MainActivity.kt
+++ app/src/main/java/com/yourcompany/myvpn/ui/MainActivity.kt
@@ -1,1 +1,50 @@
+package com.yourcompany.myvpn.ui
 
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.yourcompany.myvpn.R
+import com.yourcompany.myvpn.service.V2RayVpnService
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnStart: Button
+    private lateinit var tvStatus: TextView
+    private var isRunning = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnStart = findViewById(R.id.btn_start)
+        tvStatus = findViewById(R.id.tv_status)
+
+        btnStart.setOnClickListener {
+            if (isRunning) {
+                stopVpn()
+            } else {
+                startVpn()
+            }
+        }
+    }
+
+    private fun startVpn() {
+        val intent = Intent(this, V2RayVpnService::class.java)
+        intent.action = V2RayVpnService.ACTION_START
+        startService(intent)
+        isRunning = true
+        btnStart.text = "Остановить"
+        tvStatus.text = "Подключено"
+    }
+
+    private fun stopVpn() {
+        val intent = Intent(this, V2RayVpnService::class.java)
+        intent.action = V2RayVpnService.ACTION_STOP
+        startService(intent)
+        isRunning = false
+        btnStart.text = "Старт"
+        tvStatus.text = "Отключено"
+    }
+}
