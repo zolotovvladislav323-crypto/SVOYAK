8b9ab521fbb062e3f05abd7b9e838bd5eb3d0c07
zolotovvladislav323-crypto <zolotovvladislav323@gmail.com>
2026-08-18 15:07:32 +0300

    Update BootReceiver.kt

--- app/src/main/java/com/yourcompany/myvpn/ui/service/receiver/BootReceiver.kt
+++ app/src/main/java/com/yourcompany/myvpn/ui/service/receiver/BootReceiver.kt
@@ -1,1 +1,15 @@
+package com.yourcompany.myvpn.receiver
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.yourcompany.myvpn.service.V2RayVpnService
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            val serviceIntent = Intent(context, V2RayVpnService::class.java)
+            context.startService(serviceIntent)
+        }
+    }
+}
