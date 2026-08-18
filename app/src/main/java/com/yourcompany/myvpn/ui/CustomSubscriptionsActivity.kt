48ed8bb9d8236969f38bbc497edb5a53c6e0bae8
zolotovvladislav323-crypto <zolotovvladislav323@gmail.com>
2026-08-18 15:59:14 +0300

    Update CustomSubscriptionsActivity.kt

--- app/src/main/java/com/yourcompany/myvpn/ui/CustomSubscriptionsActivity.kt
+++ app/src/main/java/com/yourcompany/myvpn/ui/CustomSubscriptionsActivity.kt
@@ -1,1 +1,13 @@
+package com.yourcompany.myvpn.ui
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yourcompany.myvpn.R
+
+class CustomSubscriptionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_subscriptions)
+        supportActionBar?.title = "Пользовательские подписки"
+    }
+}
