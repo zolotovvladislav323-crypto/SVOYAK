6e57ea5f52e7f6464928ab4ae3a7fa320338a243
zolotovvladislav323-crypto <zolotovvladislav323@gmail.com>
2026-08-18 15:06:24 +0300

    Update V2RayTestService.kt

--- app/src/main/java/com/yourcompany/myvpn/ui/service/V2RayTestService.kt
+++ app/src/main/java/com/yourcompany/myvpn/ui/service/V2RayTestService.kt
@@ -1,1 +1,13 @@
+package com.yourcompany.myvpn.service
 
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class V2RayTestService : Service() {
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        // Тестовая логика
+        return START_NOT_STICKY
+    }
+    override fun onBind(intent: Intent?): IBinder? = null
+}
