be408188cce3d43ecfb299356de50566e0f6e49a
zolotovvladislav323-crypto <zolotovvladislav323@gmail.com>
2026-08-18 15:05:52 +0300

    Update V2RayProxyOnlyService.kt

--- app/src/main/java/com/yourcompany/myvpn/ui/service/V2RayProxyOnlyService.kt
+++ app/src/main/java/com/yourcompany/myvpn/ui/service/V2RayProxyOnlyService.kt
@@ -1,1 +1,28 @@
+package com.yourcompany.myvpn.service
 
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import libv2ray.Libv2ray
+import libv2ray.V2RayConfig
+import libv2ray.V2RayPoint
+
+class V2RayProxyOnlyService : Service() {
+    private var v2rayPoint: V2RayPoint? = null
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val config = """{"inbounds":[],"outbounds":[{"protocol":"freedom","settings":{}}]}"""
+        val v2rayConfig = V2RayConfig()
+        v2rayConfig.loadFromJSON(config)
+        v2rayPoint = Libv2ray.newV2RayPoint(v2rayConfig, false)
+        v2rayPoint?.start()
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        v2rayPoint?.stop()
+        super.onDestroy()
+    }
+
+    override fun onBind(intent: Intent?): IBinder? = null
+}
