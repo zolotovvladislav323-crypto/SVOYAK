b1198fda3bbdf171c1453eb3f583b8e782e2332f
zolotovvladislav323-crypto <zolotovvladislav323@gmail.com>
2026-08-18 16:00:26 +0300

    Update CheckUpdateActivity.kt

--- app/src/main/java/com/yourcompany/myvpn/ui/CheckUpdateActivity.kt
+++ app/src/main/java/com/yourcompany/myvpn/ui/CheckUpdateActivity.kt
@@ -1,1 +1,13 @@
+package com.yourcompany.myvpn.ui
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yourcompany.myvpn.R
+
+class CheckUpdateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_update)
+        supportActionBar?.title = "Проверка обновлений"
+    }
+}
