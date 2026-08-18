9d5232eb81745f20c0c5db6f5d7e31cd9977a561
zolotovvladislav323-crypto <zolotovvladislav323@gmail.com>
2026-08-18 15:06:48 +0300

    Update QSTileService.kt

--- app/src/main/java/com/yourcompany/myvpn/ui/service/QSTileService.kt
+++ app/src/main/java/com/yourcompany/myvpn/ui/service/QSTileService.kt
@@ -1,1 +1,13 @@
+package com.yourcompany.myvpn.service
 
+import android.service.quicksettings.TileService
+import android.content.Intent
+
+class QSTileService : TileService() {
+    override fun onClick() {
+        super.onClick()
+        val intent = Intent(this, V2RayVpnService::class.java)
+        intent.action = if (isLocked()) V2RayVpnService.ACTION_STOP else V2RayVpnService.ACTION_START
+        startService(intent)
+    }
+}
