9a8cfe73e27806a46cc9ec233aeadaf6246389ef
zolotovvladislav323-crypto <zolotovvladislav323@gmail.com>
2026-08-18 16:01:08 +0300

    Update ScSwitchActivity.kt

--- app/src/main/java/com/yourcompany/myvpn/ui/ScSwitchActivity.kt
+++ app/src/main/java/com/yourcompany/myvpn/ui/ScSwitchActivity.kt
@@ -1,1 +1,13 @@
+package com.yourcompany.myvpn.ui
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yourcompany.myvpn.R
+
+class ScSwitchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sc_switch)
+        // без action bar, translucent
+    }
+}
