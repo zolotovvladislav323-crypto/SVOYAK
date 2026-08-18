9a20d6bf8d5c552a498e7e03a65a2133c8861332
zolotovvladislav323-crypto <zolotovvladislav323@gmail.com>
2026-08-18 15:07:55 +0300

    Update TaskerReceiver.kt

--- app/src/main/java/com/yourcompany/myvpn/ui/service/receiver/TaskerReceiver.kt
+++ app/src/main/java/com/yourcompany/myvpn/ui/service/receiver/TaskerReceiver.kt
@@ -1,1 +1,14 @@
+package com.yourcompany.myvpn.receiver
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.yourcompany.myvpn.service.V2RayVpnService
+
+class TaskerReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val serviceIntent = Intent(context, V2RayVpnService::class.java)
+        serviceIntent.action = V2RayVpnService.ACTION_START
+        context.startService(serviceIntent)
+    }
+}
