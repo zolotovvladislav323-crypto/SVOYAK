954bfb4dd943e87898c154c2ecde6f9565736391
zolotovvladislav323-crypto <zolotovvladislav323@gmail.com>
2026-08-18 16:00:48 +0300

    Update UrlSchemeActivity.kt

--- app/src/main/java/com/yourcompany/myvpn/ui/UrlSchemeActivity.kt
+++ app/src/main/java/com/yourcompany/myvpn/ui/UrlSchemeActivity.kt
@@ -1,1 +1,13 @@
+package com.yourcompany.myvpn.ui
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yourcompany.myvpn.R
+
+class UrlSchemeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_url_scheme)
+        supportActionBar?.title = "Обработка ссылки"
+    }
+}
