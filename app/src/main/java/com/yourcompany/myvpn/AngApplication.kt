a34648794e96c0f178d113436a2f8c220ff3cb62
zolotovvladislav323-crypto <zolotovvladislav323@gmail.com>
2026-08-18 15:04:18 +0300

    Update AngApplication.kt

--- app/src/main/java/com/yourcompany/myvpn/AngApplication.kt
+++ app/src/main/java/com/yourcompany/myvpn/AngApplication.kt
@@ -1,1 +1,12 @@
+package com.yourcompany.myvpn
 
+import android.app.Application
+import com.tencent.mmkv.MMKV
+
+class AngApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        MMKV.initialize(this)
+        // Здесь можно инициализировать другие компоненты
+    }
+}
