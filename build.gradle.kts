6b82d08eff2501ab8d2ad0b63fe16f244f98746d
zolotovvladislav323-crypto <zolotovvladislav323@gmail.com>
2026-08-18 14:59:33 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,10 @@
-
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+}
+buildscript {
+    dependencies {
+        classpath(libs.gradle.license.plugin)
+    }
+}
