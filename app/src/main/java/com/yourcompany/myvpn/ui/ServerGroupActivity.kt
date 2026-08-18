379d932a46f45f70ee47da485657be099818fcb5
zolotovvladislav323-crypto <zolotovvladislav323@gmail.com>
2026-08-18 16:07:12 +0300

    Update ServerGroupActivity.kt

--- app/src/main/java/com/yourcompany/myvpn/ui/ServerGroupActivity.kt
+++ app/src/main/java/com/yourcompany/myvpn/ui/ServerGroupActivity.kt
@@ -1,1 +1,13 @@
+package com.yourcompany.myvpn.ui
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yourcompany.myvpn.R
+
+class ServerGroupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_server_group)
+        supportActionBar?.title = "Группы серверов"
+    }
+}
